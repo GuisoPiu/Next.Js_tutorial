0b80639ea7ff7bfaf71521ca1bffd905bdfe34f7
GuisoPiu <gruposiice.hr@gmail.com>
2022-03-22 21:43:42 -0600

    nada

---
+++ pages/contact/index.js
@@ -0,0 +1,36 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import { Navbar } from '../../components/Navbar'
+import styles from '../../styles/Home.module.css'
+
+export default function ContactPage() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Contact - Hector Rios</title>
+        <meta name="description" content="Contact Page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar />
+      
+      <main className={styles.main}>
+
+      <h1>Contact Page</h1>
+
+        <h1 className={styles.title}>
+          {/* ir a <a href="/about">About</a>{'  '} */}
+          Ir a <Link href='/'>Home</Link>
+           
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/contact.js</code>
+        </p>
+
+      </main>
+
+    </div>
+  )
+}
